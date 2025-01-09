bdb770b36aa2543377137de40e3860a94b10f65f
Cristian Marin <marin101998@gmail.com>
2025-01-09 17:47:44 -0500

    + fix | v10 09-01-25 update payment methods with image in module

--- Resources/lang/en/icommercepayzens.php
+++ Resources/lang/en/icommercepayzens.php
@@ -1,31 +1,32 @@
 <?php
 
 return [
-    'single' => 'Payzen',
-    'description' => 'The description module',
-    'list resource' => 'List icommercepayzens',
-    'create resource' => 'Create icommercepayzens',
-    'edit resource' => 'Edit icommercepayzens',
-    'destroy resource' => 'Destroy icommercepayzens',
-    'title' => [
-        'icommercepayzens' => 'IcommercePayzen',
-        'create icommercepayzen' => 'Create a icommercepayzen',
-        'edit icommercepayzen' => 'Edit a icommercepayzen',
-    ],
-    'button' => [
-        'create icommercepayzen' => 'Create a icommercepayzen',
-    ],
-    'table' => [
-        'description' => 'Descripcion',
-        'mode' => 'Mode',
-    ],
-    'form' => [
-    ],
-    'formFields' => [
-        
-    ],
-    'messages' => [
-    ],
-    'validation' => [
-    ]
+  'single' => 'Payzen',
+  'description' => 'The description module',
+  'iaDescription' => 'Online payment platform that allows merchants to process payments securely and efficiently. It offers various options for making transactions, including credit cards, debit cards and other payment methods. Their system is designed to meet the highest security standards, using technologies such as encryption and two-factor authentication.',
+  'list resource' => 'List icommercepayzens',
+  'create resource' => 'Create icommercepayzens',
+  'edit resource' => 'Edit icommercepayzens',
+  'destroy resource' => 'Destroy icommercepayzens',
+  'title' => [
+    'icommercepayzens' => 'IcommercePayzen',
+    'create icommercepayzen' => 'Create a icommercepayzen',
+    'edit icommercepayzen' => 'Edit a icommercepayzen',
+  ],
+  'button' => [
+    'create icommercepayzen' => 'Create a icommercepayzen',
+  ],
+  'table' => [
+    'description' => 'Descripcion',
+    'mode' => 'Mode',
+  ],
+  'form' => [
+  ],
+  'formFields' => [
+
+  ],
+  'messages' => [
+  ],
+  'validation' => [
+  ]
 ];
--- Resources/lang/es/icommercepayzens.php
+++ Resources/lang/es/icommercepayzens.php
@@ -1,33 +1,34 @@
 <?php
 
 return [
-    'single' => 'Payzen',
-    'description' => 'La descripcion del Modulo',
-    'list resource' => 'List icommercepayzens',
-    'create resource' => 'Create icommercepayzens',
-    'edit resource' => 'Edit icommercepayzens',
-    'destroy resource' => 'Destroy icommercepayzens',
-    'title' => [
-        'icommercepayzens' => 'IcommercePayzen',
-        'create icommercepayzen' => 'Create a icommercepayzen',
-        'edit icommercepayzen' => 'Edit a icommercepayzen',
-    ],
-    'button' => [
-        'create icommercepayzen' => 'Create a icommercepayzen',
-    ],
-    'table' => [
-        'description' => 'Description',
-        'mode' => 'Mode',
-        'siteId' => 'Site ID',
-        'signatureKey' => 'Signature Key'
-    ],
-    'form' => [
-    ],
-    'formFields' => [
-        
-    ],
-    'messages' => [
-    ],
-    'validation' => [
-    ]
+  'single' => 'Payzen',
+  'description' => 'La descripcion del Modulo',
+  'iaDescription' => 'Plataforma de pago en línea que permite a los comerciantes procesar pagos de manera segura y eficiente. Ofrece diversas opciones para realizar transacciones, incluyendo tarjetas de crédito, débito y otros métodos de pago. Su sistema está diseñado para cumplir con los más altos estándares de seguridad, utilizando tecnologías como la encriptación y la autenticación de dos factores.',
+  'list resource' => 'List icommercepayzens',
+  'create resource' => 'Create icommercepayzens',
+  'edit resource' => 'Edit icommercepayzens',
+  'destroy resource' => 'Destroy icommercepayzens',
+  'title' => [
+    'icommercepayzens' => 'IcommercePayzen',
+    'create icommercepayzen' => 'Create a icommercepayzen',
+    'edit icommercepayzen' => 'Edit a icommercepayzen',
+  ],
+  'button' => [
+    'create icommercepayzen' => 'Create a icommercepayzen',
+  ],
+  'table' => [
+    'description' => 'Description',
+    'mode' => 'Mode',
+    'siteId' => 'Site ID',
+    'signatureKey' => 'Signature Key'
+  ],
+  'form' => [
+  ],
+  'formFields' => [
+
+  ],
+  'messages' => [
+  ],
+  'validation' => [
+  ]
 ];
